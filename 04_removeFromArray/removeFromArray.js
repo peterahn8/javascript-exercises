463b1d20c689670c73145592b9b5fa7c81553454
Peter Ahn <peterahn8@gmail.com>
2022-11-12 02:49:02 -0500

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const oldArray = args[0];
+    const newArray = [];
+    oldArray.forEach((item) => {
+        if (args.includes(item) === false) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
