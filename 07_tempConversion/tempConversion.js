94549059d8add8b5c4442589c6d941aa225daf1c
Peter Ahn <peterahn8@gmail.com>
2022-11-12 03:30:58 -0500

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(value) {
+  return Math.round((value - 32) * (5/9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(value) {
+  return Math.round((value * (9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
