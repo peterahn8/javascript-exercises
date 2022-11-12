6a119589d23b3544bd17fbe39117c0f50fd63d37
Peter Ahn <peterahn8@gmail.com>
2022-11-12 01:31:55 -0500

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const reverseArray = string.split("").reverse().join("");
+    return reverseArray;
 };
 
 // Do not edit below this line
