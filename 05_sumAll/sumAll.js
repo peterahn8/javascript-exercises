ca58bb8c032984ddaf187c3a9824960229451527
Peter Ahn <peterahn8@gmail.com>
2022-11-12 03:14:04 -0500

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+finalSum = 0;
+if (typeof start != "number" || typeof end != "number") {
+    return "ERROR";
+}
+if (start < 0 || end < 0) {
+    return "ERROR";
+}
+if (start > end) {
+    let temp;
+    temp = start;
+    start = end;
+    end = temp;
+}
+for (start; start <= end; start++) {
+    finalSum += start;
+}
+return finalSum;
 };
 
 // Do not edit below this line
