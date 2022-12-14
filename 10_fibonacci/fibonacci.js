08175cb11c6802e306dfd91cb160c0b42836f760
Peter Ahn <peterahn8@gmail.com>
2022-12-14 15:32:18 -0500

    Complete fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(fibo) {
+    let n1 = 0,
+        n2 = 1,
+        next;
 
+    for (let i = 1; i <= fibo; i++) {
+        next = n1 + n2;
+        n1 = n2;
+        n2 = next;
+    }
+
+    if (fibo < 0) {
+        return "OOPS";
+    }
+    
+    return n1;
 };
 
 // Do not edit below this line
