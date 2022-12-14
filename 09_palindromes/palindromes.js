23f2eff906fb0761c646c4fbe5f49cb9a9bc5dee
Peter Ahn <peterahn8@gmail.com>
2022-12-14 15:15:43 -0500

    Complete palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    
+let phraseArray = [];
+phrase = phrase.toLowerCase();
+for (let i = 0; i < phrase.length; i++) {
+    phraseArray.push(phrase.charAt(i));
+}
+
+phraseArray = phraseArray.filter((char) => char != " ");
+phraseArray = phraseArray.filter((char) => char != ",");
+phraseArray = phraseArray.filter((char) => char != ".");
+phraseArray = phraseArray.filter((char) => char != "!");
+
+if (phraseArray.toString() === phraseArray.reverse().toString()) {
+    return true;
+} else {
+    return false;
+}
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
