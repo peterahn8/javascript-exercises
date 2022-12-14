f5dc585ea9f9e18c97c576ac3d90ae38ecebc722
Peter Ahn <peterahn8@gmail.com>
2022-12-14 14:13:18 -0500

    Complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((x, y) => x * y, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let factArray = [x];
+  if (x === 0) {
+    return 1;
+  }
+  for (let i = 1; i < x; i++) {
+    factArray.push(i)
+  }
+  return factArray.reduce((x, y) => x * y);
 };
 
 // Do not edit below this line
