5976d8e61a22dbc535ce6ac761de29241a6efcd0
Peter Ahn <peterahn8@gmail.com>
2022-12-14 16:44:00 -0500

    Complete getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,16 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+const getTheTitles = function() {
+    return books.map(x => x.title);
 };
 
 // Do not edit below this line
